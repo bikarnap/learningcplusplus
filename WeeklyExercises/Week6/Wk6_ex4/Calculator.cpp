46853073b88cd1280486012464b87d5cf958f603
Bikarna Pokharel <bikarnap@gmail.com>
2019-10-22 23:14:22 +0300

    Refactored - some refactoring and corrections

--- WeeklyExercises/Week6/Wk6_ex4/Calculator.cpp
+++ WeeklyExercises/Week6/Wk6_ex4/Calculator.cpp
@@ -9,36 +9,38 @@ int main() {
 	int number;
 	cout << "Please choose calculation (1=sqare root, 2=logarithm, 3=exp, 4=tan, 5=end program): ";
 	cin >> calculation; 
-	switch (calculation) {
-		if (calculation > 0 && calculation <= 5) {
-	case 1:
+	if (calculation > 0 && calculation < 5) {
 		cout << "Enter a number to perform chosen calculation: ";
 		cin >> number;
-		cout << "Square of " << number << ": " << sqrt(number);
-		break;
 
-	case 2:
-		cout << "Log of " << number << ": " << log(number);
-		break;
+		switch (calculation) {
+		case 1:
+			cout << "Square of " << number << ": " << sqrt(number);
+			break;
 
-	case 3:
-		cout << "Exponent of " << number << ": " << exp(number);
-		break;
+		case 2:
+			cout << "Log of " << number << ": " << log(number);
+			break;
 
-	case 4:
-		cout << "Tan of " << number << ": " << tan(number);
-		break;
+		case 3:
+			cout << "Exponent of " << number << ": " << exp(number);
+			break;
+
+		case 4:
+			cout << "Tan of " << number << ": " << tan(number);
+			break;
+
+		default:
+			break;
+		}
 	}
-
-	case 5: 
-		cout << "You have chosen to end the program. Program exiting...";
-		break;
-
-	default:
-		cout << "No such calculation available";
-		break;
+	else if (calculation == 5) {
+		cout << "You chose to terminate the program. Program terminating...";
 	}
-
+	else {
+		cout << "No such calculations available in this app!";
+	}
+	
 	// Calculation of the value of PI
 	double pi = 0.00; // initialiation of the value of PI
 	int userInput; 
