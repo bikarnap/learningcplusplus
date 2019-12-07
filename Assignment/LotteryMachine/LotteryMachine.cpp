f1a33f8167b352f1965e88f1fb6cff22d8d8fe51
Bikarna Pokharel <bikarnap@gmail.com>
2019-12-08 00:56:01 +0200

    Check for duplicate element in lottery array implemented

--- Assignment/LotteryMachine/LotteryMachine.cpp
+++ Assignment/LotteryMachine/LotteryMachine.cpp
@@ -8,11 +8,13 @@ using namespace std;
 // Function prototypes
 int* generateArray(int, int, int);
 int* combineArray(int*, int*, int, int);
+//bool Exists(int);
+
 
 int main() {
 	cout << "Lottery machine\n";
 	cout << "=================\n";
-
+	srand(time(0));
 	int* lotteryNumbers = generateArray(7, 1, 39);
 	cout << "\n";
 	int* bonusNumbers = generateArray(3, 1, 39);
@@ -22,38 +24,54 @@ int main() {
 }
 
 int* generateArray(int arraySize, int min, int max) {
-	
 	int* generatedArray; 
 	generatedArray = new int[arraySize];
-
-	srand(time(NULL));
-
+	
 	for (int i = 0; i < arraySize; i++) {
 		int element = rand() % max + min; 
-		generatedArray[i] = element; 
+		bool isDuplicate = false;
+		for (int j = 0; j < i+1; j++) {
+			if (j > 1) {
+				if (generatedArray[j] == element) {
+					isDuplicate = true;
+					break;
+				}
+					
+				else
+					isDuplicate = false;
+			}
+			
+		}
+
+		if (isDuplicate)
+			i -= 1;
+		else 
+			generatedArray[i] = element; 
 		cout << element <<" "; 
 	}
-
 	return generatedArray; 
-	
 }
 
-int *combineArray(int* array1, int * array2, int size1, int size2) {
+int* combineArray(int* array1, int * array2, int size1, int size2) {
 	int array1Size = size1;
 	int array2Size = size2;
 	int* combinedArray;
 	combinedArray = new int[array1Size + array2Size] ;
-	
 	cout << "\n";
-	for (int i = 0; i < array1Size + array2Size; i++) {
-		srand(rand() % 39 + 1);
+	for (int i = 0; i < (array1Size + array2Size); i++) {
 		if (i < array1Size)
 			combinedArray[i] = array1[i];
 		else
 			combinedArray[i] = array2[i - array1Size];
-
 		cout << combinedArray[i] << " ";
 	}
-
 	return combinedArray;
 }
+
+/*bool Exists(int arrayToCheck, int element) {
+
+	if (number)
+		return true;
+	else
+		return false;
+}*/
