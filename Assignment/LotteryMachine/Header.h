da54ead30fdc127c6baa12102ea01a563db6ed22
Bikarna Pokharel <bikarnap@gmail.com>
2019-12-10 01:17:58 +0200

    New functions added

--- Assignment/LotteryMachine/Header.h
+++ Assignment/LotteryMachine/Header.h
@@ -4,6 +4,11 @@ int* generateArray(int, int, int);
 int* combineArray(int*, int*, int, int);
 void DisplayWelcome();
 void DisplayMenu();
-int SelectOption(int);
+void SelectOption(int, int, int, int);
+int* SelectNumbers(int, int, int);
+void DisplayNumbers(int*, int);
+void DisplayResult(int*, int*, int, int, int);
+int* CompareArray(int*, int*, int);
+void Start(int, int, int, int);
 #endif // !HEADER_H
 
